a07c750ca5e3822983a63c17e63486db4b64937c
emiliano-casas <emilianogcasas@gmail.com>
2023-06-19 21:25:27 +0200

    Messing with audio

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -1,33 +1,70 @@
 import Phaser from "phaser";
+import { toASCII } from "punycode";
 
 class GameScene extends Phaser.Scene {
 
-    oSlime;
+    aNotes = []
 
     preload() {
         this.load.image('background', './assets/background.jpg');
         this.preloadWiz();
         this.preloadSlime();
+        this.load.audio('metro', './assets/metro_180bpm_5min.mp3');
+        this.preloadTrack();
     }
 
-    create() {        
-        this.add.image(320, 320, 'background');
-        this.add.text(80, 520, "--------------------------------------------------");
-        this.add.text(80, 320, "--------------------------------------------------");
-        this.createWiz();
-        this.createSlime(); 
-    }
-
-
-    // --------------------------------------------
-    // SLIME
-    // --------------------------------------------
     preloadSlime() {
         this.load.atlas(
             'slimeSprite',
             './assets/slime.png',
             './assets/slime.json'
         )
+    }
+
+    preloadWiz() {
+        // Idle
+        this.load.atlas(
+            'wizIdleRightSprite',
+            "./assets/WizIdleRight.png",
+            "./assets/WizIdleRight.json"
+        );
+        // Attack2
+        this.load.atlas(
+            'wizAttack2RightSprite',
+            "./assets/WizAttack2Right.png",
+            "./assets/WizAttack2Right.json"
+        );
+    }
+
+    create() {
+        this.add.image(320, 320, 'background');
+        this.add.text(80, 320, "--------------------------------------------------");
+        this.add.text(80, 520, "--------------------------------------------------");
+        this.add.text(80, 544, "--------------------------------------------------");
+
+        this.createWiz();
+        // this.createSlime();
+
+        this.createTrack();
+    }
+
+    update(time, delta) {
+        // console.log("time");
+        // console.log(time);
+        // console.log("delta");
+        // console.log(delta);
+    }
+
+    createTrack() {
+        const music = this.sound.add('metro');
+
+        const timeline = this.add.timeline([
+            {
+                at: 0
+            }
+        ]);
+
+        music.play();
     }
 
     createSlime() {
@@ -44,7 +81,7 @@ class GameScene extends Phaser.Scene {
                     end: 10,
                     suffix: '.png'
                 }),
-            duration: 1100,
+            duration: 1000,
         });
         this.anims.create({
             key: 'slimeBounce',
@@ -56,8 +93,8 @@ class GameScene extends Phaser.Scene {
                     end: 16,
                     suffix: '.png'
                 }),
-            duration: 600,
-            repeat: 9
+            duration: 500,
+            repeat: 16
         });
         this.anims.create({
             key: 'slimeDie',
@@ -70,9 +107,8 @@ class GameScene extends Phaser.Scene {
                     suffix: '.png'
                 }),
             duration: 800,
-            // hideOnComplete: true // Should delete slime object after this
         });
-        
+
         this.slimeSpawnLoop();
     }
 
@@ -81,40 +117,28 @@ class GameScene extends Phaser.Scene {
         // Create slime object
         // --------------------------------------------------------------
         const oSlime = this.physics.add.sprite(320, 320, 'slimeSprite');
+        oSlime.index = this.aNotes.push(oSlime) - 1;
+        console.log(this.aNotes);
+
         oSlime.on('animationcomplete-slimeDie', () => {
+            this.aNotes.splice(oSlime.index, 1);
             oSlime.destroy();
         });
-        
-        // --------------------------------------------------------------
-        // Set Animation Triggers
-        // --------------------------------------------------------------
-        oSlime.play('slimeSpawn')
-            .anims.chain('slimeBounce')
-            .anims.chain('slimeDie')
-        
-        // --------------------------------------------------------------
-        // Movement
-        // --------------------------------------------------------------
-        oSlime.setVelocity(20, 30);
+        oSlime.on('animationcomplete-slimeSpawn', () => {
+            oSlime.setVelocity(20, 40);
+            oSlime.play('slimeBounce');
+        });
+        oSlime.on('animationcomplete-slimeBounce', () => {
+            console.log("slime index " + oSlime.index);
+            this.aNotes.splice(oSlime.index, 1);
+            oSlime.destroy();
+        });
+        oSlime.play('slimeSpawn');
+
+        // setTimeout(this.slimeSpawnLoop.bind(this), 2500); // DELETE THIS
+
     }
 
-    // --------------------------------------------
-    // WIZARD
-    // --------------------------------------------
-    preloadWiz() {
-        // Idle
-        this.load.atlas(
-            'wizIdleRightSprite',
-            "./assets/WizIdleRight.png",
-            "./assets/WizIdleRight.json"
-        );
-        // Attack2
-        this.load.atlas(
-            'wizAttack2RightSprite',
-            "./assets/WizAttack2Right.png",
-            "./assets/WizAttack2Right.json"
-        );
-    }
 
     createWiz() {
         // --------------------------------------------------------------
